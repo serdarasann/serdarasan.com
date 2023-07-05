e4a20a7b4f52d59f8dd8f77acfe90cf69565c2cc
Serdar AŞAN <serdarasann@gmail.com>
2023-07-05 22:27:41 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/header'
+import Slider from './components/slider'
+import About from './components/about'
 
 function App() {
   return (
     <>
-      ...
+        <Header/>
+        <Slider/>
+        <About/>
     </>
   );
 }
